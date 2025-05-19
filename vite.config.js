db4e1ddced1538f76bb4426f531f22e9cc5bf035
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2025-05-18 23:39:47 -0500

    added deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/micro-encabulator-site/", // <-- use your repo name here, with slashes
+});

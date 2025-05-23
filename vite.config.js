7e52171c7d395ec7454120803344a2938928794d
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2025-05-23 10:27:40 -0500

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-});
+  base: command === 'build' ? '/micro-encabulator-site/' : '/',
+}));
